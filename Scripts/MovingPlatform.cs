4bbff55bd4affcf5568b8be4693a28662bd5fed7
Empteyy <57791584+Empteyy@users.noreply.github.com>
2019-11-15 11:52:29 +0100

    Add files via upload

---
+++ Scripts/MovingPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public Vector3 destination;
+    public float speed;
+    Vector3 source;
+    Vector3 moveTo;
+
+    // Use this for initialization
+    void Start()
+    {
+        source = transform.position;
+        moveTo = destination;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveTo, Time.deltaTime * speed);
+        if (transform.position == destination)
+            moveTo = source;
+        if (transform.position == source)
+            moveTo = destination;
+    }
+}
---
+++ Scripts/SceneSelectScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    public void selectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "LEVEL 1":
+                SceneManager.LoadScene("OWO");
+                break;
+            case "LEVEL 2":
+                SceneManager.LoadScene("Level 2");
+                break;
+            case "ReturnButton":
+                SceneManager.LoadScene("Main Menu");
+                break;
+            case "Respawn1":
+                SceneManager.LoadScene("OWO");
+                break;
+            case "Respawn2":
+                SceneManager.LoadScene("Level 2");
+                break;
+            case "Main menu":
+                SceneManager.LoadScene("Main Menu");
+                break;
+            case "Levels":
+                SceneManager.LoadScene("LevelSelectionMainMenu");
+                break;
+        }
+    }
+ }
+
+
